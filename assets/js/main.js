84fc2d8c01037e3a11d8e70ed2cac3986b72be4b
Khiêm Hoàng Sỹ <3121410263@sv.sgu.edu.vn>
2025-02-18 14:47:46 +0700

    contact, project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,60 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(element => {
+    element.addEventListener('click', linkAction)
+});
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== SHADOW HEADER ====== =========*/
 
 
 /*=============== EMAIL JS ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+document.querySelectorAll('.contact__input').forEach(input => {
+    input.addEventListener('focus', () => {
+      input.style.transition = 'none'; // Tắt hiệu ứng khi focus
+      setTimeout(() => {
+        input.style.transition = ''; // Bật lại hiệu ứng sau khi focus
+      }, 10);
+    });
+  });
+  
